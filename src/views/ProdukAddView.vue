1db7c95d029890225d0242c7f64f469f87275c03
malascoding <asepabdulmugni22@gmail.com>
2024-11-02 11:19:36 +0700

    add Nabvbar Component to Produk Add

--- src/views/ProdukAddView.vue
+++ src/views/ProdukAddView.vue
@@ -1,5 +1,25 @@
 <template>
-  <section>
+  <!-- NAVBAR -->
+  <Navbar :name="name" :role_id="role_id" />
+  <!--END NAVBAR -->
+  <section class="py-20">
     <h1>Produk Add</h1>
   </section>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      name: localStorage.getItem("name"),
+      role_id: localStorage.getItem("role_id"),
+    };
+  },
+  mounted() {},
+};
+</script>
